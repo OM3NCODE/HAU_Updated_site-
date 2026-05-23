63fa0af890df9f580913df3efc2547e0ef2dea7a
Adhish Bharadwaj [PC] <officialadhishbharadwaj@gmail.com>
2026-05-23 17:05:50 +0530

    updated config file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Replace 'your-repo-name' with the exact name of your GitHub repository
   base: '/HAU_Updated_site/', 
 })
